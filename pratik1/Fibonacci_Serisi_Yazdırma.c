d8f83490721cddb5f5af214686aabd787090620b
Muhammed Furkan <122174098+mfurkanozdemir@users.noreply.github.com>
2024-12-12 22:21:10 +0300

    Update and rename Fibonacci Serisi Yazdırma to Fibonacci_Serisi_Yazdırma.c

---
+++ pratik1/Fibonacci_Serisi_Yazdırma.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int n, t1 = 0, t2 = 1, nextTerm;
+    printf("Kaç terim istiyorsunuz? ");
+    scanf("%d", &n);
+    printf("Fibonacci Serisi: %d, %d", t1, t2);
+    for (int i = 3; i <= n; ++i) {
+        nextTerm = t1 + t2;
+        printf(", %d", nextTerm);
+        t1 = t2;
+        t2 = nextTerm;
+    }
+    printf("\n");
+    return 0;
+}
+
